55689073c67c5a4692af2776f6cfce42a0aaa334
Hasan Emre Karabacak <hsn.dev@hotmail.com>
2025-04-07 19:55:30 +0300

    initialize project with React, Vite, and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+
+
+    return (
+        <>
+            <h1 className='bg-amber-300'>Project Started</h1>
+        </>
+    )
+}
+
+export default App
